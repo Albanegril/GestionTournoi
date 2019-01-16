87aff8f72691f6a8303089b83db1022b197fb4f0
Tropee Leo <leo.tropee@gmail.com>
2019-01-16 12:20:17 +0100

    test illegal/ repository

--- GestionTournoi/src/main/java/com/ensim/GestionTournoi/Controller/AffichageTournoiController.java
+++ GestionTournoi/src/main/java/com/ensim/GestionTournoi/Controller/AffichageTournoiController.java
@@ -1,22 +1,36 @@
 package com.ensim.GestionTournoi.Controller;
 
 
-import org.json.JSONObject;
+import com.ensim.GestionTournoi.Model.ModelDAO.TournoiRepository;
+import com.ensim.GestionTournoi.Model.Tournoi;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Optional;
 
 @Controller
 public class AffichageTournoiController
 {
+    @Autowired
+    TournoiRepository tournoiRepository;
+
     @GetMapping("/affichageTournoi")
     public String arbre(@RequestParam(name="id",defaultValue = "1") int id, Model model) {
-        //String tournoi
+        String tournoi;
+        Optional<Tournoi> tour = tournoiRepository.findById(id);
+        if(tour.isPresent())
+        {
+            tournoi = tour.get().getJson();
+        }
+        else
+        {
+            tournoi = "{}";
+        }
 
-        //model.addAttribute("tournoi",tournoi);
+        model.addAttribute("tournoi",tournoi);
         return "arbre";
 
     }
---
+++ GestionTournoi/src/main/java/com/ensim/GestionTournoi/Model/ModelDAO/TournoiRepository.java
@@ -0,0 +1,9 @@
+package com.ensim.GestionTournoi.Model.ModelDAO;
+
+import com.ensim.GestionTournoi.Model.Tournoi;
+import org.springframework.data.repository.CrudRepository;
+
+public interface TournoiRepository extends CrudRepository<Tournoi, Integer>
+{
+
+}
